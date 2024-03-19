7a1a39fa2135a31fe63a5a7035aff0677abd3441
Abhishek Ajmera <abhishekajmera17@gmail.com>
2024-03-19 22:31:22 +0530

    .

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,6 +1,24 @@
-const StoreItem = () => {
-  return (
-    <div>StoreItem</div>
-  )
-}
-export default StoreItem
+import { Card } from 'react-bootstrap';
+
+const StoreItem = ({ category, description, image, rating, price, title }: dataType) => {
+	return (
+		<Card>
+			<Card.Img
+				variant='top'
+				src={image}
+				alt={title}
+				height={180}
+				style={{ objectFit: 'contain' }}
+			/>
+			<Card.Body className='d flex flex-column'>
+				<Card.Title className='d-flex justify-content-between align-items-baseline mb-4'>
+					<span className='col-9'>{title}</span>
+					<span className='col-3 text-end'>$ {price}</span>
+				</Card.Title>
+			</Card.Body>
+			<p>{category}</p>
+			<p>{description}</p>
+		</Card>
+	);
+};
+export default StoreItem;
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,18 +1,35 @@
 import { Col, Row } from 'react-bootstrap';
 import StoreItem from '../components/StoreItem';
+import { useEffect, useState } from 'react';
 
 const Store = () => {
+	const [data, setData] = useState<null | dataType[]>(null);
+
+	useEffect(() => {
+		async function fetchData() {
+			const res = await fetch('https://fakestoreapi.com/products');
+			const dataRes = await res.json();
+			setData(dataRes);
+			// console.log(dataRes);
+		}
+		fetchData();
+	}, []);
+
 	return (
 		<>
 			<h2>Store</h2>
 			<Row
 				md={2}
 				xs={1}
+				lg={3}
 				className='g-3'
 			>
-				<Col>
-					<StoreItem />
-				</Col>
+				{data &&
+					data.map((data) => (
+						<Col key={data.id}>
+							<StoreItem {...data} />
+						</Col>
+					))}
 			</Row>
 		</>
 	);
