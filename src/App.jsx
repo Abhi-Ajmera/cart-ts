96af50bd05a02403a78cf3d907105cf027cce229
Abhishek Ajmera <abhishekajmera17@gmail.com>
2024-03-27 11:56:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from './components/Navbar';
+
+function App() {
+	return (
+		<div className="className='w-full h-screen bg-zinc-900 text-white">
+			<Navbar />
+		</div>
+	);
+}
+
+export default App;
